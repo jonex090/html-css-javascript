908f101fb351bfe896e64b1ebdd1cba50daf994d
Jovica Ilieski <jonex090@gmail.com>
2023-06-19 23:14:39 +0200

    something

--- script1.js
+++ script1.js
@@ -1,16 +1,26 @@
-fetch('https://fakestoreapi.com/products').then((data) => {
+fetch('https://jsonplaceholder.typicode.com/users?fbclid=IwAR1hvQEmF50APk8AOn_YSNELNZJrfxibZnW3VltHasg0o3qlkY0b7OA020U').then((data) => {
     // console.log(data)
     return data.json();
 }).then((completedata) => {
     // console.log(completedata);
     let data1 = "";
     completedata.map((vrednost) => {
-        data1+= ` <div class="card">
-        <h1 class="title">${vrednost.title}</h1>
-        <img src=${vrednost.image} alt="img" class="images">
-        <p>${vrednost.description}</p>
-        <p class="category">${vrednost.category}</p>
-        <p class="price">${vrednost.price}</p>
+        data1+= `     <div class="card">
+        <ul>
+            <li>ID: ${vrednost.id}</li>
+            <li>Name: ${vrednost.name}</li>
+            <li>UserName: ${vrednost.username}</li>
+            <li>Email: ${vrednost.email}</li>
+            <li>Address</li>
+            <p>Street: ${vrednost.address.street}</p>
+            <p>City: ${vrednost.address.city}</p>
+            <p>ZipCode: ${vrednost.address.zipcode}</p>
+            <li>Phone: ${vrednost.phone}</li>
+            <li>Website: ${vrednost.website}</li>
+            <li>Company: ${vrednost.company.name}</li>
+            <p>${vrednost.company.catchPhrase}</p>
+            <p>${vrednost.company.bs}</p>
+        </ul> 
     </div> `
     });
     document.getElementById("cards").innerHTML=data1;
